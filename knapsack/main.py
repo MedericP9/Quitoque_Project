ad0b45d15f8a69469f0d4f136c4a79ab0bf040f0
Médéric PENIGUEL <78308034+MedericP9@users.noreply.github.com>
2024-12-18 11:05:57 +0100

    Update main.py

--- knapsack/main.py
+++ knapsack/main.py
@@ -28,8 +28,7 @@ def knapsack_func(weights, values, capacity):
     print(len(weights), len(values), capacity)
     # Pour utiliser pyomo
     solvername = "glpk"
-    solver_path = setup_folder_path + "winglpk-4.65/glpk-4.65/w64/glpsol"
-
+    solver_path = "knapsack/setup/winglpk-4.65/glpk-4.65/w64/glpsol.exe"
     # Création du modèle
     model = ConcreteModel()
 
