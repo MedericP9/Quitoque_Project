760780c3474ddc75cfde2ac360ffd6ab3ada2a50
Médéric PENIGUEL <78308034+MedericP9@users.noreply.github.com>
2024-12-19 00:05:22 +0100

    Update Partie_2_Modelisation.py

--- pages/Partie_2_Modelisation.py
+++ pages/Partie_2_Modelisation.py
@@ -4,7 +4,6 @@ st.sidebar.write("Made by Médéric PENIGUEL")
 st.sidebar.write("My [linkedIn profil](https://www.linkedin.com/in/mederic-peniguel)")
 st.sidebar.write("My [Email](mailto:mederic.peniguel@efrei.net)")
 
-st.title("PARTIE 2 : Modélisation")
 st.markdown(
     """
     <h2 style='text-align: center; font-size: 28px; color: #4CAF50;'>
