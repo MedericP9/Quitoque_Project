3ba9b25e7d1bfeb562b2116ca0519e9de0d8183c
Médéric PENIGUEL <78308034+MedericP9@users.noreply.github.com>
2024-12-18 13:22:50 +0100

    Update main.py

--- knapsack/main.py
+++ knapsack/main.py
@@ -79,7 +79,7 @@ def knapsack_func(weights, values, capacity):
 
     model.constraint = Constraint(rule=capacity_constraint_rule)
     # Résolution du modèle
-    opt = SolverFactory(solvername, executable=solver_path, set_executable=False)
+    opt = SolverFactory("glpk")
     results = opt.solve(model)
 
     ## Résultats
