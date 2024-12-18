134bf344012f9816c786f2032fdc6b450893f4d3
Médéric PENIGUEL <78308034+MedericP9@users.noreply.github.com>
2024-12-18 15:15:38 +0100

    Update main.py

--- knapsack/main.py
+++ knapsack/main.py
@@ -4,34 +4,36 @@ from pulp import *
 
 def knapsack_func(weights, values, capacity):
 
-    n = len(weights)
-    
-    # Création de la matrice pour la programmation dynamique
-    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+n = len(items)
 
-    # Remplissage de la matrice dp
-    for i in range(1, n + 1):
-        for w in range(capacity + 1):
-            if weights[i - 1] <= w:
-                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1])
-            else:
-                dp[i][w] = dp[i - 1][w]
-    
-    # Récupération des objets sélectionnés
-    selected_items = []
-    w = capacity
-    for i in range(n, 0, -1):
-        if dp[i][w] != dp[i - 1][w]:
-            selected_items.append(i - 1)
-            w -= weights[i - 1]
-    
-    # Créer un DataFrame avec les informations des objets
-    items_data = {
-        "Item": [i for i in range(n)],
-        "Weight": weights,
-        "Value": values
-    }
-    df = pd.DataFrame(items_data)
+# Table de programmation dynamique
+dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+
+# Remplissage de la table
+for i in range(1, n + 1):
+    for w in range(capacity + 1):
+        if weights[i - 1] <= w:
+            dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1])
+        else:
+            dp[i][w] = dp[i - 1][w]
+
+# Valeur maximale obtenue
+max_value = dp[n][capacity]
+
+# Récupération des objets sélectionnés
+w = capacity
+selected_items = []
+for i in range(n, 0, -1):
+    if dp[i][w] != dp[i - 1][w]:
+        selected_items.append(i - 1)
+        w -= weights[i - 1]
+
+# Création du DataFrame
+df = pd.DataFrame({
+    'Item': [items[i] for i in selected_items],
+    'Weight': [weights[i] for i in selected_items],
+    'Value': [values[i] for i in selected_items]
+})
 
     # Ajouter une colonne 'SELECTED' pour indiquer si l'objet est sélectionné
     df['SELECTED'] = df.index.isin(selected_items)
