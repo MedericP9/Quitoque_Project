2d0f3a6450a81290d3ae406fd9b97660c9f9c496
Médéric PENIGUEL <78308034+MedericP9@users.noreply.github.com>
2024-12-19 00:35:42 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -17,7 +17,7 @@ st.sidebar.write("My [Email](mailto:mederic.peniguel@efrei.net)")
 
 st.markdown("""
     <h2 style='text-align: center; font-size: 28px; color: #4CAF50;'>
-        Bienvenue sur ma candidature pour le poste de Data MLOps chez Quitoque
+        Bienvenue sur ma candidature pour le poste de Data Scientist / ML Ops Engineer (H/F) – Stage 6 mois chez Quitoque
     </h2>
     """, unsafe_allow_html=True)
 
@@ -26,18 +26,15 @@ st.header("À propos de moi")
 st.write("""
 Bonjour,
 
-Je suis Médéric PENIGUEL, passionné par les données et les opérations de machine learning. 
-Fort d'une expérience en [votre expérience pertinente], je suis enthousiaste à l'idée de contribuer à l'innovation culinaire de Quitoque.
+Je suis Médéric PENIGUEL, passionné par l’intelligence artificielle, l’analyse de données et l’utilisation de technologies innovantes pour résoudre les problèmes complexes. Compétent en Mathématiques, Python, Machine learning et deep learning , je recherche un stage de fin d’études pour mettre mes compétences techniques et analytiques au service de projets ambitieux en data engineering et IA. Rigoureux, organisé et créatif, je souhaite contribuer activement en équipe à des solutions innovantes. je suis enthousiaste à l'idée de contribuer à l'innovation culinaire de Quitoque.
 """)
 
 
 # Navigation vers les différentes sections
 st.header("Explorez mon travail")
 st.write("""
-- **PARTIE 1 : SQL**  
-  Découvrez mes compétences en requêtes SQL avancées.
+- **PARTIE 1 : Présentation des requêtes SQL**  
 - **PARTIE 2 : Modélisation**  
-  Plongez dans mes approches de modélisation pour la prédiction de la demande.
-- **PARTIE 3 : Knapsack**  
-  Testez mon implémentation interactive de l'algorithme du sac à dos.
+- **PARTIE 3 : Programmation, Optimisation du Sac à Dos**  
+- **PARTIE 4 : Questions annexes**  
 """)
